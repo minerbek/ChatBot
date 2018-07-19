90a118080bf01db33b8044a5081a91f17b5f9b42
mine <mineerbek@outlook.com>
2018-07-19 20:02:33 +0300

    ilk gonderme

---
+++ bot.py
@@ -0,0 +1,46 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import argparse
+import logging
+import warnings
+
+from rasa_core import utils
+from rasa_core.actions import Action
+from rasa_core.agent import Agent
+from rasa_core.channels.console import ConsoleInputChannel
+from rasa_core.events import SlotSet
+from rasa_core.featurizers import (
+    MaxHistoryTrackerFeaturizer,
+    BinarySingleStateFeaturizer)
+from rasa_core.interpreter import RasaNLUInterpreter
+from rasa_core.policies.memoization import MemoizationPolicy
+
+logger = logging.getLogger(__name__)
+
+
+def run(serve_forever=True):
+    interpreter = RasaNLUInterpreter("models/nlu/default/current")
+    agent = Agent.load("models/dialogue", interpreter=interpreter)
+
+    if serve_forever:
+        agent.handle_channel(ConsoleInputChannel())
+    return agent
+
+
+if __name__ == '__main__':
+    utils.configure_colored_logging(loglevel="INFO")
+
+    parser = argparse.ArgumentParser(
+            description='starts the bot')
+
+    parser.add_argument(
+            'task',
+            choices=["run"],
+            help="what the bot should do - e.g. run or train?")
+    task = parser.parse_args().task
+
+    # decide what to do based on first parameter of the script
+    run()
